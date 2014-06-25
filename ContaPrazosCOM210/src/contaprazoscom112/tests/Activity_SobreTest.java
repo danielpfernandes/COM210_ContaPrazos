df7de52472a130a06dbf1511a3adaaa51eb42be5
danielpfernandes <daniel.paivafernandes@gmail.com>
2014-06-25 18:00:04 -0300

    Adição de Casos de Teste 3
    Activity_LoginTest, Activity_SobreTest, Acticity_VisProcessoTest,
    Activity_VisUsuarioTest

---
+++ ContaPrazosCOM210/src/contaprazoscom112/tests/Activity_SobreTest.java
@@ -0,0 +1,32 @@
+package contaprazoscom112.tests;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+import com.example.contaprazoscom112.*;
+import com.example.contaprazoscom112.R;
+
+public class Activity_SobreTest extends ActivityInstrumentationTestCase2<Activity_Sobre> {
+
+				
+	private Activity_Sobre mActivity_Sobre;
+	private TextView mFirstTestText;
+	
+	public Activity_SobreTest() {
+		super(Activity_Sobre.class);
+	}
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+			mActivity_Sobre = getActivity();
+			mFirstTestText = (TextView) mActivity_Sobre.findViewById(R.id.textView1);
+	}
+	
+	public void testActivity_Configuracoes_labelText() {
+		final String expected =
+				mActivity_Sobre.getString(R.string.hello_world);
+		final String actual = mFirstTestText.getText().toString();
+		assertEquals(expected, actual);
+	}
+}
