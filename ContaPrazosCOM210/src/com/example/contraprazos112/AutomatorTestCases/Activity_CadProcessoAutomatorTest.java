f35483dc51a850d14a519a68de602b672bc53e69
danielpfernandes <daniel.paivafernandes@gmail.com>
2014-06-26 11:09:13 -0300

    Substituição do Activity_CadProcessoAutomatorTest
    Substituição pelo Automator Test Case Launch ContaPrazos

--- ContaPrazosCOM210/src/com/example/contraprazos112/AutomatorTestCases/Activity_CadProcessoAutomatorTest.java
+++
@@ -1,9 +0,0 @@
-package com.example.contraprazos112.AutomatorTestCases;
-
-import com.android.uiautomator.testrunner.UiAutomatorTestCase;
-
-public class Activity_CadProcessoAutomatorTest extends UiAutomatorTestCase {
-	
-	
-}
-
